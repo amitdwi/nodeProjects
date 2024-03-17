8932fe70139dcf96a75eca97597acdc9e0143832
amitdwi <amitdubey.108@gmail.com>
2024-03-18 01:24:21 +0530

    Added login & register functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 
 require('dotenv').config();
 
@@ -9,6 +10,7 @@ require('./db');
 
 app.use(bodyParser.json());
 app.use('/users', userRoutes);
+app.use('/tasks', taskRoutes);
 
 app.get('/', (req, res) => {
     res.json({
